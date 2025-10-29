6ccc70dcf8b5cfc7de02c11ab60273a655e58a9f
saantydev <saantydev@gmail.com>
2025-10-29 18:49:04 -0300

    Update refresh.js

--- refresh.js
+++ refresh.js
@@ -1,8 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-refreshDatos();
-refreshId = setInterval(refreshDatos, 10000)
-})
-function refreshDatos(cont){
-let valor = document.querySelector("#temp-int")
-valor.innerHTML = (parseInt(valor.innerHTML) + 1)
+    refreshDatos();
+    refreshId = setInterval(refreshDatos, 10000);
+});
+
+function refreshDatos() {
+    let valorTemp = document.querySelector("#temp-int");
+    valorTemp.innerHTML = (parseInt(valorTemp.innerHTML) + 1);
+
+    let valorHumedad = document.querySelector("#humedad-int");
+    valorHumedad.innerHTML = (parseInt(valorHumedad.innerHTML) + 1);
+
+    let valorViento = document.querySelector("#viento-int");
+    valorViento.innerHTML = (parseInt(valorViento.innerHTML) + 1);
+
+    let valorIncendio = document.querySelector("#incendio-int");
+    let statusIncendio = document.querySelector("#incendio-status");
+
+    let incendioActual = parseInt(valorIncendio.innerHTML);
+    if (incendioActual >= 1) {
+        valorIncendio.innerHTML = 0;
+        statusIncendio.innerHTML = "No Detectado";
+    } else {
+        valorIncendio.innerHTML = 1;
+        statusIncendio.innerHTML = "Detectado";
+    }
 }
